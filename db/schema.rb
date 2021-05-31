1c29dd6416ed9d25aabb997eb772ccfbbe021c99
Pasha Bratanov <zihzag@gmail.com>
2021-05-31 21:48:06 +0300

    #3: set uniqueness of email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_181625) do
+ActiveRecord::Schema.define(version: 2021_05_31_183907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,13 +7,19 @@ RSpec.describe User, type: :model do
   end
 
   it 'should NOT be valid without email' do
-    user = User.new(email: 'example@mail.com', token_expires: '12-03-2021')
+    user = User.new(token_expires: '12-03-2021')
     expect(user).to_not be_valid
   end
 
   it 'should NOT be valid with non unique email' do
     User.create(email: 'example@mail.com')
     user = User.new(email: 'example@mail.com', token_expires: '12-03-2021')
+    expect(user).to_not be_valid
+  end
+
+  it 'should NOT be valid with non unique email' do
+    User.create(email: 'example@mail.com')
+    user = User.new(email: 'Example@mail.com', token_expires: '12-03-2021')
     expect(user).to_not be_valid
   end
 end
