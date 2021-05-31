507ced0073eedd2f4bcb8f4b349209fed60c0ea9
Pasha Bratanov <zihzag@gmail.com>
2021-05-31 21:30:20 +0300

    #3: set uuid as primary keys and create User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'should be valid without token' do
+    user = User.new(email: 'example@mail.com', token_expires: '12-03-2021')
+    expect(user).to be_valid
+  end
+
+  it 'should NOT be valid without email' do
+    user = User.new(email: 'example@mail.com', token_expires: '12-03-2021')
+    expect(user).to_not be_valid
+  end
+
+  it 'should NOT be valid with non unique email' do
+    User.create(email: 'example@mail.com')
+    user = User.new(email: 'example@mail.com', token_expires: '12-03-2021')
+    expect(user).to_not be_valid
+  end
+end
