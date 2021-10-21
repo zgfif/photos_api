aadf20f8dd85bfe4fbb68b3fad8bf38783689e14
Pasha Bratanov <zihzag@gmail.com>
2021-10-21 09:13:24 +0300

    #20: resolve bug

--- spec/lib/search_pictures_spec.rb
+++ spec/lib/search_pictures_spec.rb
@@ -6,12 +6,19 @@ RSpec.describe SearchPictures do
   it 'should return picture attributes' do
     term = 'Pentax'
     result = SearchPictures.new(term).search
-    expect(result.count).to eq(7)
+    expect(result.count).to eq(5)
   end
 
   it 'should not return any pictures' do
     term = 'nothing'
     result = SearchPictures.new(term).search
     expect(result.count).to eq(0)
+  end
+
+  it 'should return only one picture by id' do
+    term = '58f45d1fff468c0848e2'
+    result = SearchPictures.new(term).search
+
+    expect(result.count).to eq(1)
   end
 end
--- spec/lib/search_result_spec.rb
+++ spec/lib/search_result_spec.rb
@@ -3,11 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe SearchResult do
+  def params(term)
+    ActionController::Parameters.new({ term: term }).permit(:term)
+  end
+
   it 'should return images by author' do
     term = 'Victorious'
-    params = ActionController::Parameters.new({ term: term }).permit(:term)
-
-    result = SearchResult.new(params).process
+    result = SearchResult.new(params(term)).process
 
     expect(result[:pictures].count).to eq(1)
     expect(result[:pictures][0][:author]).to include(term)
@@ -15,9 +17,7 @@ RSpec.describe SearchResult do
 
   it 'should NOT find any image by author' do
     term = 'invald'
-    params = ActionController::Parameters.new({ term: term }).permit(:term)
-
-    result = SearchResult.new(params).process
+    result = SearchResult.new(params(term)).process
 
     expect(result[:pictures].count).to eq(0)
   end
